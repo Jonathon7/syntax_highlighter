8cbd9e5cdf62db33dfc67334729a4874d2bf22f7
Jonathon Flores <jonathon@Jonathons-MacBook-Pro.local>
2019-05-21 16:33:59 -0500

    updated webpack config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     filename: "index.js"
   },
   module: {
